cb18268d17e927bf1cdd82c5022227c67c042a99
ouadieelallaly-netizen <ouadieelallaly@gmail.com>
2026-04-13 13:31:26 +0100

    php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'Chapitre 1 – Connexion à une base de données avec PDO.php';
+
+try {
+    $sql = "SELECT * FROM user";
+    $stmt = $pdo->query($sql);
+
+    $utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($utilisateurs as $user) {
+        echo "ID : " . $user['id'] . " - name : " . $user['name'] . " - Email : " . $user['email'] . "<br>";
+    }
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+echo "<table border='1'>
+<tr><th>ID</th><th>Nom</th><th>Email</th></tr>";
+foreach ($utilisateurs as $user) {
+    echo "<tr><td>{$user['id']}</td><td>{$user['name']}</td><td>{$user['email']}</td></tr>";
+}
+echo "</table>";
